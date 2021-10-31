6f48bfbf9be541cc54e121a93560efaadb7be88c
Yudhistira Wibowo <10561001+itsmeyaw@users.noreply.github.com>
2021-11-01 00:30:22 +0100

    Changed the iam json

--- deploy/iam.tf
+++ deploy/iam.tf
@@ -1,20 +1,20 @@
 resource "aws_iam_role" "lambda_function_iam" {
   name               = "${var.project_name}-lambda-role"
   assume_role_policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Action": "sts:AssumeRole",
-        "Principal": {
-          "Service": "lambda.amazonaws.com"
-        },
-        "Effect": "Allow",
-        "Sid": ""
-      }
-    ]
-  }
-  EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 }
 
 resource "aws_iam_policy" "ses_policy" {
